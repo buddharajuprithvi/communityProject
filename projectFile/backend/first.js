e1ee23f8849dbad05c7794c28eb2e1fba2f3a1bb
Prithvi <prithvi.buddharaju@kuliza.com>
2018-09-18 01:25:12 +0530

    login post api sample done

--- projectFile/backend/first.js
+++ projectFile/backend/first.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var myParser = require("body-parser");
 const app = express()
 const port = 4000
 
@@ -14,5 +15,26 @@ app.get('/home', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     return res.send(JSON.stringify(json));
 })
+
+app.use(myParser.urlencoded({ extended: true }));
+app.use(myParser.json());
+
+app.post('/login', function (request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    console.log("request : ", typeof request.body);
+    if ((request.body.name == 'prithvi' || request.body.name == 'chandu') && request.body.password == 'testing') {
+        return response.send(JSON.stringify({
+            data: {
+                ...request.body,
+                token: 'qwerty123',
+            },
+            status: 200,
+            message: 'success',
+        }));
+    }
+    return response.send(JSON.stringify({
+        errorMessage: 'invalied login credentials',
+    }));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
